8209d5596d36e71d8f9ee451fd35d91d4ff72c53
Dhruv Sagar <dhruv.sagar27@icloud.com>
2023-11-23 20:23:46 -0500

    added Custom method

--- app/src/main/java/com/datadissolve/util/DataSanitization.java
+++ app/src/main/java/com/datadissolve/util/DataSanitization.java
@@ -8,6 +8,7 @@ import java.security.SecureRandom;
  * The Gutmann method overwrites data 35 times with 8 different patterns.
  * The DoD method overwrites data 7 times with 4 different patterns.
  * The Schneier method overwrites data 7 times with 4 different patterns.
+ * The Custom method overwrites data n times with m different patterns.
  */
 public class DataSanitization {
     public static void wipeDataGutmann(byte[] data) {
@@ -73,6 +74,24 @@ public class DataSanitization {
         new SecureRandom().nextBytes(pattern7);
         for (int i = 0; i < pattern3.length; i++) {pattern4[i] = (byte) ~pattern3[i];}
         byte[][] patterns = {pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7};
+
+        for (byte[] pattern : patterns) {
+            for (int j = 0; j < data.length; j += pattern.length) {
+                for (int k = 0; k < pattern.length; k++) {
+                    if (j + k < data.length) {
+                        data[j + k] = pattern[k];
+                    }
+                }
+            }
+        }
+        Arrays.fill(data, (byte) 0x00);
+    }
+
+    public static void wipeDataCustom(byte[] data, int numOfPatterns, int patternSize) {
+        byte[][] patterns = new byte[numOfPatterns][patternSize];
+        for (int i = 0; i < numOfPatterns; i++) {
+            new SecureRandom().nextBytes(patterns[i]);
+        }
 
         for (byte[] pattern : patterns) {
             for (int j = 0; j < data.length; j += pattern.length) {
